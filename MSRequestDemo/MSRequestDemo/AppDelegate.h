5a99b363e0f0b4564a97581326f9baab6f5e336e
kangbo <lala.kang1989@gmail.com>
2017-08-28 17:31:12 +0800

    src submit

---
+++ MSRequestDemo/MSRequestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSRequestDemo
+//
+//  Created by meishi on 28/08/2017.
+//  Copyright © 2017 Kangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MSRequestDemo/MSRequestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MSRequestDemo
+//
+//  Created by meishi on 28/08/2017.
+//  Copyright © 2017 Kangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
